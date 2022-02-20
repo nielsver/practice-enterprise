bb12653355936269b3f424a425b85d39861c98db
nielsver <nielsvervoort14@gmail.com>
2022-02-20 12:18:15 +0100

    logfile +project application

---
+++ Android3/Android3/app/src/main/java/com/Android3/Android3.java
@@ -0,0 +1,21 @@
+
+package com.Android3;
+
+import android.app.Activity;
+import android.widget.TextView;
+import android.os.Bundle;
+
+public class Android3 extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        /* Create a TextView and set its text to "Hello world" */
+        TextView  tv = new TextView(this);
+        tv.setText("test");
+        setContentView(tv);
+    }
+}
