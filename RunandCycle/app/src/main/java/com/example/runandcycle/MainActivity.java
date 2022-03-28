4806a0c949599fde4a542544dac6eca4dd0a1618
nielsver <nielsvervoort14@gmail.com>
2022-03-28 10:18:32 +0200

    home update

--- RunandCycle/app/src/main/java/com/example/runandcycle/MainActivity.java
+++ RunandCycle/app/src/main/java/com/example/runandcycle/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         binding.toolbar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Already on home page", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
