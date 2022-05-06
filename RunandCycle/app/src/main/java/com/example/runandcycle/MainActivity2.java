5c3bb0b15472e90d1ce3a0ec1ea2c86c75958061
nielsver <nielsvervoort14@gmail.com>
2022-05-06 19:01:07 +0200

    startnewactivityrunpage

--- RunandCycle/app/src/main/java/com/example/runandcycle/MainActivity2.java
+++ RunandCycle/app/src/main/java/com/example/runandcycle/MainActivity2.java
@@ -1,6 +1,7 @@
 package com.example.runandcycle;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -59,7 +60,8 @@ public class MainActivity2 extends AppCompatActivity {
         Run.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dialog.dismiss();
+                Intent i = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(i);
             }
         });
     }
