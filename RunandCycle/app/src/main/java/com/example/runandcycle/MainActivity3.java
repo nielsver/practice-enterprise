8379dc17497cedbcd707fe9390dc6056bfdb5a88
nielsver <nielsvervoort14@gmail.com>
2022-05-07 14:11:47 +0200

    not working timer update

--- RunandCycle/app/src/main/java/com/example/runandcycle/MainActivity3.java
+++ RunandCycle/app/src/main/java/com/example/runandcycle/MainActivity3.java
@@ -1,15 +1,83 @@
 package com.example.runandcycle;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity3 extends AppCompatActivity {
+    TextView timertext;
+    Button finish;
+    Button stop;
+    Timer timer;
+    TimerTask timertask;
+    Double time = 0.0;
+    private boolean timerRunning = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
         getSupportActionBar().setTitle("recording");
+        timertext = findViewById(R.id.timer);
+        finish = findViewById(R.id.finish);
+        stop = findViewById(R.id.stop);
+        timer = new Timer();
+        startTimer();
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startStopTapped(view);
+            }
+        });
+    }
+
+
+    public void startStopTapped(View view) {
+        if (!timerRunning) {
+            timerRunning = true;
+            stop.setText("@string/Stop");
+            startTimer();
+        } else {
+            timerRunning = false;
+            stop.setText("@string/Start");
+            timertask.cancel();
+        }
+    }
+
+    private void startTimer() {
+        timertask = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        time++;
+                        timertext.setText(getTimerText());
+                    }
+                });
+
+            }
+        };
+        timer.scheduleAtFixedRate(timertask, 0, 1000);
+
+    }
+
+    private String getTimerText() {
+        int rounded = (int) Math.round(time);
+        int seconds = ((rounded % 86400) % 3600) % 60;
+        int minutes = ((rounded % 86400) % 3600) / 60;
+        int hours = ((rounded % 86400) / 3600);
+
+        return formatTime(seconds, minutes, hours);
+    }
+
+    private String formatTime(int seconds, int minutes, int hours) {
+        return String.format("%02d", hours + " : " + "%02d", minutes + " : " + "%02d", seconds);
     }
 }
